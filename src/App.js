23036f60bdde8067c4c9cd5dd9b4c475dfec585b
izaak svec <slavi.svec@gmail.com>
2021-09-14 17:18:36 +1000

    Add Github query, fetch API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
-import React from 'react'
 import GithubData from './GithubData'
+import React, { useEffect } from 'react'
 
 const App = () => {
-  return <div className="App">Search Github Repositories</div>
+  useEffect(() => {
+    const gitHubDataQuery = {
+      query: `
+    {
+        viewer {
+          repositories(privacy: PUBLIC, last: 10) {
+            totalCount
+            nodes {
+              name
+            }
+          }
+        }
+    }
+        `
+    }
+    fetch(GithubData.baseURL, {
+      method: 'POST',
+      headers: GithubData.headers,
+      body: JSON.stringify(gitHubDataQuery)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.data.viewer.repositories.nodes)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  })
+
+  return (
+    <div className="container">
+      <h1 className="repositories-heading">Repositories list</h1>
+    </div>
+  )
 }
 
 export default App
