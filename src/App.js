e7f3f0f4b4ceac34d03b6866e01f25e23a084eee
izaak svec <slavi.svec@gmail.com>
2021-09-18 14:02:56 +1000

    Add repos list from array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useCallback } from 'react'
-import GithubData from './GithubData'
-import githubDataQuery from './GithubQuery'
+import GithubData from './GitHubData/GithubData.jsx'
+import githubDataQuery from './GithubQuery/GithubQuery.jsx'
+import './App.css'
 
 const App = () => {
   const [githubUserName, setGithubUserName] = useState('')
@@ -15,23 +16,43 @@ const App = () => {
       .then(response => response.json())
       .then(data => {
         setGithubUserName(data.data.viewer.name)
-        setGithubReposList(data.data.viewer.repositories.nodes[0].name)
-        console.log(data.data.viewer.repositories.nodes[0].name)
+        setGithubReposList(data.data.viewer.repositories.nodes)
       })
       .catch(error => {
         console.log(error)
       })
-  }, [])
+  }, [githubReposList])
 
   useEffect(() => {
     fetchData()
-  }, [fetchData])
+  }, []) //   This is an example of the response data
+
+  const nodes = [
+    {
+      name: 'crypto-tracker'
+    },
+    {
+      name: 'interval-timer'
+    },
+    {
+      name: 'Slavi-Svec'
+    },
+    {
+      name: 'react-repo-searcher'
+    }
+  ]
 
   return (
     <div className="container">
-      <h1 className="repositories-heading">Repositories list</h1>
-          <p>This is {githubUserName.trim() + "'s"} Github repository</p>
-      <div>here is my github repo -> {githubReposList}</div>
+       <h1 className="repositories-heading">Repositories list</h1>
+         <p>This is {githubUserName.trim() + "'s"} Github repository</p>
+      <ul className="github-repo-list">
+        <li className="github-repo-list-items">
+          {nodes.map(user => (
+            <div className="github-repo-titles">{user.name}</div>
+          ))}
+        </li>
+      </ul>
     </div>
   )
 }
