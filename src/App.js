ff4c9b4180448f7c7441b9bbcc1240c884387312
izaak svec <slavi.svec@gmail.com>
2021-10-11 10:32:06 +1100

    Update App.js file with query

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
+import githubData from './GithubData/GithubData'
+import githubDataQuery from './GithubDataQuery'
+import './App.css'
 
-function App() {
-  return <div>Hello World</div>
+const App = () => {
+  const [githubUserName, setGithubUserName] = useState('')
+  const [githubReposList, setGithubReposList] = useState('')
+
+  const fetchData = useCallback(() => {
+    fetch(githubData.baseURL, {
+      method: 'POST',
+      headers: githubData.headers,
+      body: JSON.stringify(githubDataQuery)
+    })
+      .then(response => response.json())
+      .then(data => {
+        setGithubUserName(data.data.viewer.name)
+        setGithubReposList(data.data.viewer.repositories.nodes)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
+  return (
+    <div className="container">
+      <h1 className="repositories-heading">Repositories list</h1>
+      {githubUserName && (
+        <p>This is {`${githubUserName.trim()}'s`} Github repository</p>
+      )}
+      <ul className="github-repo-list">
+        <li className="github-repo-list-items">
+          {githubReposList &&
+            githubReposList.map(user => (
+              <div className="github-repo-titles">{user.name}</div>
+            ))}
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default App
